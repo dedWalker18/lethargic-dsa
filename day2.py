2bed23e9dd1eb0730f270e1a51e211895ce34bd7
dedWalker18 <ganeriwala01@gmail.com>
2024-05-05 21:01:48 +0530

    day2 part 2

--- day2.py
+++ day2.py
@@ -1,6 +1,7 @@
-####################
-# Part 1 for Day 2 #
-####################
+##########################
+#    Part 1 for Day 2    #
+# Also Part2 apparentlly #
+##########################
 
 ## uses some more regex
 ## lot of optimisation scope here
@@ -29,26 +30,33 @@ def get_color_counts(game_string):
         color_counts[color] = max(color_counts.get(color, 0), count)
     return color_counts
 
+def get_product(color_counts):
+    return color_counts["green"] * color_counts["red"] * color_counts["blue"]
+
 def check_games(games): 
+    sum = 0
     for game in games:
         if game == "":
             break
             
-        all_nums = re.findall(r'\d+',game)
-        flag = (True for num in all_nums if int(num) > 15)
-        if not flag:
-            continue
+        # all_nums = re.findall(r'\d+',game)
+        # flag = (True for num in all_nums if int(num) > 15)
+        # if not flag:
+        #     continue                                         ## uncomment this snippet for part 1
             
         color_counts = get_color_counts(game)
-        if check_possibility(color_counts):
-            game = re.findall(r'Game\s\d+', game)
-            id.append(int(re.findall(r'\d+', game[0])[0]))
-            
-id = []
+        
+        # if check_possibility(color_counts):
+        #     game = re.findall(r'Game\s\d+', game)
+        #     id.append(int(re.findall(r'\d+', game[0])[0]))   ## uncomment this snippet for part 1
+        
+        sum += get_product(color_counts)
+    
+    return sum
 
+id = []
 with open('read.txt', "r") as file:
     input = file.read()
 
 games = input.split("\n")
-check_games(games)
-print(sum(id))
+print(check_games(games))
